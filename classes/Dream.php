0dc91f5aca0663cd241cd45cec03a42411c08148
Tiavina Ranaivoson <a.tiavina.ran@gmail.com>
2023-05-06 20:46:03 +0400

    add get methods in all entities

--- classes/Dream.php
+++ classes/Dream.php
@@ -153,4 +153,67 @@ class DreamService {
 
         $DB->delete('dreams', 'id = ' . $id);
     }
+
+    public static function getAll($data = array(), $where = '')
+    {
+        $ids = self::getAllIds($data, $where);
+        $list = array();
+
+        foreach ($ids as $id) {
+            $list[] = new Dream($id);
+        }
+
+        return $list;
+    }
+
+    public static function getAllIds($data = array(), $where = '')
+    {
+        global $DB;
+
+        $where = self::getAllWhere($data, $where);
+
+        return $DB->getCol('SELECT id FROM dreams ' . $where['where'] . ' ORDER BY id DESC', $where['params']);
+    }
+
+    public static function getAllCount($data = array(), $where = '')
+    {
+        global $DB;
+
+        $where = self::getAllWhere($data, $where);
+
+        return $DB->getVar('SELECT COUNT(*) FROM dreams ' . $where['where'] . ' ORDER BY id DESC', $where['params']);
+    }
+
+    public static function getAllWhere($data = array(), $where = '')
+    {
+        $params = array();
+        $wheres = array();
+
+        if (is_array($data)) {
+            // TODO: 
+        }
+
+        if ($where != '') {
+            $wheres[] = $where;
+        }
+
+        return array(
+            'where' => count($wheres) > 0 ? ' WHERE ' . implode(' AND ', $wheres) : '',
+            'params' => $params
+        );
+    }
+
+    public static function get($id, $col)
+    {
+        global $DB;
+
+        return $DB->getVar('SELECT ' . $col . ' FROM dreams WHERE id = :id LIMIT 1', array('id' => $id));
+    }
+
+    public static function getIdFrom($col, $val)
+    {
+        global $DB;
+
+        return $DB->getVar('SELECT id FROM dreams WHERE ' . $col . ' = :' . $col . ' LIMIT 1', array($col => $val));
+    }
 }
