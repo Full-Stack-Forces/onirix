878cabe90f8e2e7b9cd6711d604ed668069dd6ae
Tiavina Ranaivoson <a.tiavina.ran@gmail.com>
2023-05-07 02:16:34 +0400

    add some links

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -1,1 +1,2 @@
 <span>La moyenne des utilisateurs ayant eu un cauchemar est de: <?php echo round((1 - $average) * 100, 2) ?>%</span>
+<a href="/ads">Gérer les publicités</a>
--- views/layout/header.php
+++ views/layout/header.php
@@ -8,6 +8,11 @@ if (isset($_SESSION['user'])) {
     echo '<span>' . $_SESSION['user']->email() . '</span>';
     echo '<a href="/my-dreams">Mes rêves</a>';
     echo '<a href="/profile">Mon profil</a>';
+    
+    if ($_SESSION['user']->isAdmin()) {
+        echo '<a href="/dashboard">Tableau de bord</a>';
+    }
+
     echo '<a href="/logout">Se déconnecter</a>';
 } else {
     echo '<a href="/login">Se connecter</a>';
