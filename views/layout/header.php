86929a87d63c5869bd8bbaccee01bbe76dd3cb64
Tiavina Ranaivoson <a.tiavina.ran@gmail.com>
2023-05-07 05:15:04 +0400

    optimize style of header

--- views/layout/header.php
+++ views/layout/header.php
@@ -1,5 +1,5 @@
 <header>
-    <nav class="bg-white border-gray-200 dark:bg-gray-900">
+    <nav class="glass border-gray-200 dark:bg-gray-900">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <a href="/" class="flex items-center">
                 <img src="/public/assets/img/logo-iir.svg" class="h-12 mr-3" alt="Logo de l'Institut Internationnal des Rêves" />
@@ -58,7 +58,7 @@
                 </button>
             </div>
             <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="mobile-menu-2">
-                <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                     <?php
                         $menus = array(
                             array('link' => '/', 'text' => 'Accueil'),
@@ -71,13 +71,13 @@
                             if ($menu['link'] == CURRENT_LINK) {
                                 echo '
                                     <li>
-                                        <a href="' . $menu['link'] . '" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">' . $menu['text'] . '</a>
+                                        <a href="' . $menu['link'] . '" class="block py-2 pl-3 pr-4 text-white bg-sky-950 rounded md:bg-transparent md:text-orange-400 md:p-0 aria-current="page">' . $menu['text'] . '</a>
                                     </li>
                                 ';
                             } else {
                                 echo '
                                     <li>
-                                        <a href="' . $menu['link'] . '" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">' . $menu['text'] . '</a>
+                                        <a href="' . $menu['link'] . '" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:text-white md:hover:text-white md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">' . $menu['text'] . '</a>
                                     </li>
                                 ';
                             }
