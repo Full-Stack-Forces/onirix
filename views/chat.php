fb94e1c82f7706280bb3ff7b09696816c2d07dc3
Tiavina Ranaivoson <a.tiavina.ran@gmail.com>
2023-05-07 10:32:10 +0400

    fix chat title

--- views/chat.php
+++ views/chat.php
@@ -1,5 +1,7 @@
-<h1 class="text-4xl text-center">Chat</h1>
-<div id="chat" class="container mx-auto">
+<h1 class="mt-10 text-4xl text-center">Commençons l'expérience</h1>
+<p class="text-center mt-2">
+    J'espère que tu vas bien, qu'as-tu rêvé récemment ?</p>
+<div id="chat" class="mt-5 container mx-auto">
     <div id='fake_form' class="w-3/5 flex flex-col items-center mx-auto">
         <div id="meta_keys" class="w-full p-3" style="min-height: 45vh; max-height: 45vh; overflow-y: auto;">
         </div>
@@ -14,8 +16,8 @@
             <button class="my-auto btn btn-info text-black ml-4" id="add_meta" onclick="dreamMeta()">+ Ajouter</button>
         </div>
         <form method="POST" id='form-dream'>
-        <input type="hidden" name="action" value="save_dream" />
-    </form>
+            <input type="hidden" name="action" value="save_dream" />
+        </form>
     </div>
 </div>
 <script>
@@ -27,73 +29,75 @@
 
     function getMetaKeys(id) {
         $.ajax({
-        type: "POST",             //Méthode à employer POST ou GET 
-        url: "/api",
-        data: {
-            action: 'get_meta_keys',
-            id: id
-        },
-        dataType: "json",         //Type de données à recevoir, ici, du HTML.
-        success: function (data) {
-            if (!data) { return; }
-            
-            let metaKeys = document.getElementById('meta_keys');
-            let p = document.createElement('div');
-            let div = document.createElement('div');
+            type: "POST", //Méthode à employer POST ou GET 
+            url: "/api",
+            data: {
+                action: 'get_meta_keys',
+                id: id
+            },
+            dataType: "json", //Type de données à recevoir, ici, du HTML.
+            success: function(data) {
+                if (!data) {
+                    return;
+                }
 
-            p.innerText = data;
-            p.classList.add('chat-bubble', 'bg-sky-700', 'text-white', 'shadow-md');
-            div.classList.add('chat', 'chat-start');
-            div.append(p);
-            metaKeys.append(div);
-        },
-        error: function (data) {
-            console.log(data);
-        }
-    });
+                let metaKeys = document.getElementById('meta_keys');
+                let p = document.createElement('div');
+                let div = document.createElement('div');
+
+                p.innerText = data;
+                p.classList.add('chat-bubble', 'bg-sky-700', 'text-white', 'shadow-md');
+                div.classList.add('chat', 'chat-start');
+                div.append(p);
+                metaKeys.append(div);
+            },
+            error: function(data) {
+                console.log(data);
+            }
+        });
     }
 
     function addMetaValues(content) {
         $.ajax({
-        type: "POST",             //Méthode à employer POST ou GET 
-        url: "/api",
-        data: {
-            action: 'add_meta_values',
-            value: content,
-            id: count
-        },
-        dataType: "json",         
-        success: function (data) {
-            if (!data) {
-                return;
+            type: "POST", //Méthode à employer POST ou GET 
+            url: "/api",
+            data: {
+                action: 'add_meta_values',
+                value: content,
+                id: count
+            },
+            dataType: "json",
+            success: function(data) {
+                if (!data) {
+                    return;
+                }
+
+                let metaKeys = document.getElementById('meta_keys');
+                let p = document.createElement('div');
+                let div = document.createElement('div');
+
+                p.innerText = data;
+                p.classList.add('chat-bubble', 'bg-sky-950', 'text-white', 'shadow-md');
+                div.classList.add('chat', 'chat-end');
+
+                div.append(p);
+                metaKeys.append(div);
+
+                count++;
+                getMetaKeys(count);
+
+                if (!document.getElementById('submit')) {
+                    let submit = document.getElementById('submit');
+                    submit.innerText = 'Envoyer';
+                    submit.classList.add('btn', 'rounded-full', 'bg-green-500', 'hover:bg-green-200', 'text-black');
+
+                    document.getElementById('form-dream').append(submit);
+                }
+            },
+            error: function(data) {
+                console.log(data);
             }
-
-            let metaKeys = document.getElementById('meta_keys');
-            let p = document.createElement('div');
-            let div = document.createElement('div');
-
-            p.innerText = data;
-            p.classList.add('chat-bubble', 'bg-sky-950', 'text-white', 'shadow-md');
-            div.classList.add('chat', 'chat-end');
-
-            div.append(p);
-            metaKeys.append(div);
-
-            count++;
-            getMetaKeys(count);
-            
-            if (!document.getElementById('submit')) {
-                let submit = document.getElementById('submit');
-                submit.innerText = 'Envoyer';
-                submit.classList.add('btn', 'rounded-full', 'bg-green-500', 'hover:bg-green-200', 'text-black');
-                
-                document.getElementById('form-dream').append(submit);
-            }
-        },
-        error: function (data) {
-            console.log(data);
-        }
-    });
+        });
     }
 
     function dreamMeta() {
